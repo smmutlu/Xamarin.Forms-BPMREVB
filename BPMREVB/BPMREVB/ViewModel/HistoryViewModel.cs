64682f91d91a00af1cbbe24c23abe3646bb9cef7
Unknown <mutlusean@gmail.com>
2018-05-02 02:36:11 -0700

    Added data experimental data bindings for h istory page

---
+++ BPMREVB/BPMREVB/ViewModel/HistoryViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace BPMREVB.ViewModel
+{
+    class HistoryViewModel : INotifyPropertyChanged
+    {
+        private string defaultText = "Button";
+        private string displayText = "";
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public HistoryViewModel()
+        {
+            AddNewLabel = new Command<string>((key) => { displayText += defaultText + key; });
+        } 
+    public ICommand AddNewLabel{ protected set; get; }
+                public string DisplayText
+        {
+            protected set 
+            {
+                if (displayText != value)
+                {
+                    displayText = value;
+                    OnPropertyChanged("DisplayText");
+                }
+            }
+            get { return displayText; }
+        }
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
